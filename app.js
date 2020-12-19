ef29d970e269de6363744c67d925a0532adf2593
Jerry Teng <zuyuangteng@gmail.com>
2020-12-19 17:53:01 -0600

    added location

--- app.js
+++ app.js
@@ -55,5 +55,5 @@ app.use("/account", require("./routes/account"));
 connection();
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port: ${PORT}`);
+	console.log(`Server is running at http://localhost:${8080}`);
 });
