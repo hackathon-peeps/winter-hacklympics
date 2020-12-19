54ca5a2a4628fdb106960847e4027e23cd6ffed7
JerryTeng01 <zuyuangteng@gmail.com>
2020-12-18 18:44:48 -0600

    added signal

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = new express();
 path = require("path");
+const port = 8080;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get("/login/google", (req, res) => {
-    
-})
 
-app.listen(8080);
+
+app.get("/login/google", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`);
+});
