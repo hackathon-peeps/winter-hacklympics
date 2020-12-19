945be21b6085688f7ff97062ed99b175c0fcb920
JerryTeng01 <zuyuangteng@gmail.com>
2020-12-19 13:19:08 -0600

    database connection

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "/config" });
+
+const connection = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connection;
